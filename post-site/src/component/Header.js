90aff4746a70e4b1a177ff05270d5075948215fb
kovesdieszter <eszter.kovesdi@ioflair.com>
2024-01-23 15:26:31 +0100

    feat: implement Header component

---
+++ post-site/src/component/Header.js
@@ -0,0 +1,18 @@
+function Header({ showForm, setShowForm }) {
+  return (
+    <header className="header">
+      <div className="logo">
+        <img src="logo.png" height="68" width="68" alt="Today I Learned Logo" />
+        <h1>Post it here</h1>
+      </div>
+      <button
+        className="btn btn-large btn-open"
+        onClick={() => setShowForm((show) => !show)}
+      >
+        {showForm ? "Close" : "Share a fact"}
+      </button>
+    </header>
+  );
+}
+
+export default Header;
