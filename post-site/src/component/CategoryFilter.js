def68f94a32df41a26608ccff924617d4def81e8
kovesdieszter <eszter.kovesdi@ioflair.com>
2024-01-23 15:31:12 +0100

    feat: implement CategoryFilter component - without actions

---
+++ post-site/src/component/CategoryFilter.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { CATEGORIES } from "../data/data";
+
+function CategoryFilter() {
+  return (
+    <aside>
+      <ul>
+        <li className="category">
+          <button className="btn btn-all-categories">All</button>
+        </li>
+        {CATEGORIES.map((cat) => (
+          <li key={cat.name} className="category">
+            <button
+              className="btn btn-category"
+              style={{ backgroundColor: cat.color }}
+            >
+              {cat.name}
+            </button>
+          </li>
+        ))}
+      </ul>
+    </aside>
+  );
+}
+
+export default CategoryFilter;
