2a0715d813a21598ed1f4a47df6d0e6243a6c49e
kovesdieszter <eszter.kovesdi@ioflair.com>
2024-01-23 15:31:57 +0100

    feat: update index.js & App.js

--- post-site/src/App.js
+++ post-site/src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./style.css";
+import Header from "./component/Header";
+import CategoryFilter from "./component/CategoryFilter";
+import NewFactForm from "./component/NewFactForm";
+import FactList from "./component/FactList";
+import { useEffect, useState } from "react";
+import supabase from "./supabase";
 
 function App() {
+  const [showForm, setShowForm] = useState(false);
+  const [facts, setFacts] = useState([]);
+
+  useEffect(function () {
+    async function getFacts() {
+      const { data: facts, error } = await supabase.from("facts").select("*");
+      setFacts(facts);
+    }
+    getFacts();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header showForm={showForm} setShowForm={setShowForm} />
+      {showForm ? (
+        <NewFactForm setFacts={setFacts} setShowForm={setShowForm} />
+      ) : null}
+      <main className="main">
+        <CategoryFilter />
+        <FactList facts={facts} />
+      </main>
+    </>
   );
 }
 
