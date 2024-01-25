8dfe85b4be7b3237cdb8e6e403d6432a6d392845
kovesdieszter <eszter.kovesdi@ioflair.com>
2024-01-25 23:55:45 +0100

    fix: remove console.log

--- post-site/src/supabase.js
+++ post-site/src/supabase.js
@@ -1,5 +1,4 @@
 import { createClient } from "@supabase/supabase-js";
-console.log(process.env);
 const supabaseUrl = "https://kcvmirwpcremaoizgtaj.supabase.co";
 const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
