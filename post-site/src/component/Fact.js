d0b6830cbca4ffe5e2507944d3455426cc1387d7
kovesdieszter <eszter.kovesdi@ioflair.com>
2024-01-26 00:26:57 +0100

    fix: minor fixes

--- post-site/src/component/Fact.js
+++ post-site/src/component/Fact.js
@@ -4,6 +4,8 @@ import { useState } from "react";
 
 function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
+  const isDisputed =
+    fact.votesInteresting + fact.votesMindblowing < fact.votesFalse;
 
   async function handleVote(columnName) {
     setIsUpdating(true);
@@ -22,6 +24,7 @@ function Fact({ fact, setFacts }) {
   return (
     <li className="fact">
       <p>
+        {isDisputed ? <span className="disputed">[DISPUTED]</span> : null}
         {fact.text}
         <a className="source" href={fact.source} target="_blank">
           (Source)
